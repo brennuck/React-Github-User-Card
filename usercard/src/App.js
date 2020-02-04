e344dfdea2a1cfa51a3aa2aea5e9502e55f2b51c
Brennon Nuckols <brennuck@gmail.com>
2020-02-03 20:13:21 -0700

    Fetch the User Data

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { setState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: []
+  };
+
+  componentDidMount() {
+    axios.get("https://api.github.com/users/brennuck")
+    .then(res => {
+      console.log("Response" ,res);
+      this.setState({
+        users: res.data
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>hello</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
